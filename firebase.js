d69df590ed33c30b690c58f829b4220432b7446d
avi7332 <139627628+avi7332@users.noreply.github.com>
2023-07-29 11:40:57 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqTB-8B_6llvsfhidKKynpXTMsrk1AVhk",
+  authDomain: "zxfrt-f7fe4.firebaseapp.com",
+  databaseURL: "https://zxfrt-f7fe4-default-rtdb.firebaseio.com",
+  projectId: "zxfrt-f7fe4",
+  storageBucket: "zxfrt-f7fe4.appspot.com",
+  messagingSenderId: "57171762243",
+  appId: "1:57171762243:web:da3b1c81e8ca26b4df48f6"
+};
+
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
